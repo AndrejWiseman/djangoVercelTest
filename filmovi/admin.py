c7914141d113306a7dddce59d32d4bfba39a4eed
Andrej <533737@gmail.com>
2024-05-07 16:35:35 +0200

    Initial commit

---
+++ filmovi/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Filmovi
+
+
+class FilmoviAdmin(admin.ModelAdmin):
+    list_display = ['naslov']
+
+
+# Register your models here.
+admin.site.register(Filmovi, FilmoviAdmin)
